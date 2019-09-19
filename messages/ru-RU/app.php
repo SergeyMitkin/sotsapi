1a85787ce18b3974017363862f6d53e5ff1486b3
Pravenkaia <pravlen@rukzak.ru>
2019-09-19 12:53:12 +0300

    feet(modules/rbac): add RBAC-module
    module can edit roles and rules for users
    
    Full descriptions https://github.com/developeruz/yii2-db-rbac/blob/master/README.RU.md

---
+++ messages/ru-RU/app.php
@@ -0,0 +1,33 @@
+<?php
+
+return[
+    'Home' => 'Домой',
+    'Calendar' => 'Календарь',
+    'Activities' => 'События',
+    'Users' => 'Пользователи',
+    'Title' => 'Заголовок',
+    'Create Activity' => 'Содать событие',
+    'Personal account' => 'Личный кабинет',
+    'My activities' => 'Мои события',
+    'My account' => 'Мой аккаунт',
+    'Registration' => 'Регистрация',
+    'Authorization' => 'Авторизация',
+    'Logout' => 'Выйти',
+    'Date Start' => 'Дата начала',
+    'Date End' => 'Дата конца',
+    'Date start' => 'Дата начала',
+    'Date end' => 'Дата конца',
+    'Description' => 'Описание',
+    'Is Repeatable' => 'Повторяется',
+    'Is Blocking' => 'Блокирющее',
+    'Description:<br> {description}' => 'Описание:<br> {description}',
+    'Author' => 'Автор',
+    'Date Created' => 'Дата создания',
+    'Date Changed' => 'Дата изменения',
+    'Activity created date: {0} {1}' => 'Дата создания события {0} {1}',
+    'Date start: {0, date, dd.MM.yyy}' => 'Дата начала: {0, date, dd.MM.yyy}',
+    'Date start: {0}' => 'Дата начала: {0, date, dd.MM.yyy}',
+    '' => '',
+    'Update' => 'Обновить'
+
+];
